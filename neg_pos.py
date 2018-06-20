6c98248478b18e8caa5344eee06b2b08bd736757
riavats05 <40036467+riavats05@users.noreply.github.com>
2018-06-20 14:02:00 +0530

    Add files via upload

---
+++ neg_pos.py
@@ -0,0 +1,8 @@
+print "Check whether the number is +ve or -ve:"
+num=(int)(raw_input("Enter a number: "))
+if num==0:
+	print "Number is 0"
+elif num<0:
+	print "Number is -ve"
+else:
+	print "Number is +ve"
